5ceadadcde9cb3725fc7b222a87288c573a3ca68
pezzin <christian.pezzin@gmail.com>
2026-07-06 08:38:28 +0200

    Fix: mount auth routes on root for /login access

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use(session({
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
 
-app.use('/auth', authRoutes);
+app.use('/', authRoutes);
 app.use('/api', apiRoutes);
 
 app.get('/', requireAuth, (req, res) => {
